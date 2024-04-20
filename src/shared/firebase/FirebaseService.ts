c17425ea33109fe3975a95018e09a28561740e27
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2024-04-19 23:03:52 -0300

    remove recaptcha

--- src/shared/firebase/FirebaseService.ts
+++ src/shared/firebase/FirebaseService.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-restricted-imports
 import { FirebaseApp, getApp, getApps, initializeApp } from 'firebase/app';
-import { ReCaptchaV3Provider, initializeAppCheck } from 'firebase/app-check';
 import { firebaseConfig } from './firebaseConfig';
 
 const setupFirebase = (): FirebaseApp => {
@@ -9,10 +8,6 @@ const setupFirebase = (): FirebaseApp => {
   }
 
   const firebaseApp = initializeApp(firebaseConfig);
-  initializeAppCheck(firebaseApp, {
-    provider: new ReCaptchaV3Provider('6LfBZ7IpAAAAAF4q_HespEbH5Vz87TWyZPPOCfDh'),
-    isTokenAutoRefreshEnabled: true,
-  });
   return firebaseApp;
 };
 
