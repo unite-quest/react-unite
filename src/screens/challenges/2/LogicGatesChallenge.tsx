a213c57fdd1666039efbefa80ce8d295de99e449
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2024-04-20 15:01:46 -0300

    fix bundle

--- src/screens/challenges/2/LogicGatesChallenge.tsx
+++ src/screens/challenges/2/LogicGatesChallenge.tsx
@@ -1,7 +1,7 @@
 import { ChallengeFooter } from '@/components/shell/ChallengeFooter';
 import { ChallengeScreen } from '@/components/shell/ChallengeScreen';
 import { useNavigate } from 'react-router-dom';
-import logicGatesHard from '../../assets/logic-gates-hard.png';
+import logicGatesHard from '../../../assets/logic-gates-hard.png';
 
 function LogicGatesChallenge() {
   const navigate = useNavigate();
