f63be9e82f8e645aedf9e155f16e536c96019da0
unknown <mimi.vki@gmail.com>
2024-05-11 21:13:12 -0300

    feat: credits images

--- src/shared/utils/creditsEntries.ts
+++ src/shared/utils/creditsEntries.ts
@@ -9,35 +9,37 @@ export const creditEntries: CreditEntry[] = [
   {
     label: 'Direction',
     value: ['Mimi', 'Gabriel'],
-    image: 'https://gabrieltnishimura.github.io/unite/challenges-intro/intro-maze.webp',
+    image: 'https://gabrieltnishimura.github.io/unite/credits/credits1.webp',
     color: 'text-[#1616EF]',
   },
   {
     label: 'Game Design',
     value: ['Mimi', 'Gabriel', 'Humayta'],
-    image: 'https://gabrieltnishimura.github.io/unite/challenges-intro/intro-maze.webp',
+    image: 'https://gabrieltnishimura.github.io/unite/credits/credits2.webp',
     color: 'text-[#1414C8]',
   },
   {
     label: 'UI/UX Design',
     value: ['Humayta'],
-    image: 'https://gabrieltnishimura.github.io/unite/challenges-intro/intro-maze.webp',
     color: 'text-[#0F0FD1]',
   },
   {
     label: 'Pixel Art',
     value: ['Matheus Bitencourt', 'ChatGPT', 'Midjourney', 'Mimi'],
     color: 'text-[#1919B3]',
+    image: 'https://gabrieltnishimura.github.io/unite/credits/credits3.webp',
   },
-  { label: 'Development', value: ['Mimi', 'Gabriel'], color: 'text-[#027599]' },
+  { label: 'Development', value: ['Mimi', 'Gabriel'], color: 'text-[#027599]'}, 
   {
     label: 'Alpha Testing',
     value: ['Paty', 'Luiza', 'Jessica'],
     color: 'text-[#026482]',
+    image: 'https://gabrieltnishimura.github.io/unite/credits/credits4.webp'
   },
   {
     label: 'Beta Testing',
     value: ['Caique', 'Carol', 'Claudia', 'Gianfranco', 'Guilherme', 'Fabio', 'Rodrigo', 'Roberto'],
+    image: 'https://gabrieltnishimura.github.io/unite/credits/credits5.webp',
     color: 'text-[#024D66]',
   },
   { label: 'Players', value: ['Voce'], color: 'text-[#015F7A]' },
